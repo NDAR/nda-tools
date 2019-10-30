56b90c65f3471ba8bf8a628bfeff008a80f24d25
Roger <roger.lee@nih.gov>
2019-10-30 13:49:21 -0400

    Updated version for prod deploy to PyPI.

--- NDATools/__init__.py
+++ NDATools/__init__.py
@@ -3,7 +3,7 @@ import requests
 import json
 import sys
 
-__version__ = '0.1.41'
+__version__ = '0.1.21'
 pypi_version = None
 version_checked = False
 
@@ -13,7 +13,7 @@ def check_version():
     except ImportError:
         from pip._vendor.packaging.version import parse
     # use https://test.pypi.org/pypi/{package}/json on test/release branches, use https://pypi.org on master
-    url_pattern = 'https://test.pypi.org/pypi/{package}/json'
+    url_pattern = 'https://pypi.org/pypi/{package}/json'
     package = 'nda-tools'
     """Return version of package on pypi.python.org using json."""
     req = requests.get(url_pattern.format(package=package))
