13f6410a29f25d8c851e2a24cc770a096448bcd0
Nancy Schroepfer <nancy.schroepfer@nih.gov>
2025-05-16 11:24:58 -0500

    REV-1424 fix tests

--- tests/upload/submission/test_associated_file.py
+++ tests/upload/submission/test_associated_file.py
@@ -9,27 +9,23 @@ from NDATools.upload.submission.api import SubmissionStatus, Submission, NdaColl
     AssociatedFile, AssociatedFileStatus, AssociatedFileUploadCreds
 from NDATools.upload.submission.associated_file import AssociatedFileUploader
 
-submission_id = 89075
-collection_id = 80
-associated_file_id1 = 111
-associated_file_id2 = 222
 
 @pytest.fixture
 def get_submission():
-    collection = NdaCollection(id=collection_id, title='best cupcake collection')
+    collection = NdaCollection(id=80, title='best cupcake collection')
     submission = Submission(submission_status=SubmissionStatus.UPLOADING, dataset_title='best cupcake data',
                             dataset_description='survey about the best cupcake research',
-                            dataset_created_date='05-14-2025', dataset_modified_date=None, submission_id=submission_id,
+                            dataset_created_date='05-14-2025', dataset_modified_date=None, submission_id=78905,
                             collection=collection)
     return submission
 
 
 @pytest.fixture
 def get_associated_files():
-    associated_file1 = AssociatedFile(id=associated_file_id1, file_user_path='README.txt',
+    associated_file1 = AssociatedFile(id=111, file_user_path='README.txt',
                                       file_remote_path='s3://nda-central-dev/collection-80/submission-89075/associated-files/README',
                                       status=AssociatedFileStatus.READY, size=2074)
-    associated_file2 = AssociatedFile(id=associated_file_id2, file_user_path='README2.txt',
+    associated_file2 = AssociatedFile(id=222, file_user_path='README2.txt',
                                       file_remote_path='s3://nda-central-dev/collection-80/submission-89075/associated-files/README2',
                                       status=AssociatedFileStatus.READY, size=2074)
     return [associated_file1, associated_file2]
@@ -52,12 +48,12 @@ def test_start_upload_happy_path(mock_get_s3_client, get_submission, get_associa
 
     mock_submission_api.get_files_by_page.side_effect = [[associated_file1], [associated_file2]]
 
-    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file_id1,
+    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file1.id,
                                               destination_uri=associated_file1.file_remote_path,
                                               source_uri=associated_file1.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
                                               session_token='DFDFKSADL3452340980')
-    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file_id2,
+    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file2.id,
                                               destination_uri=associated_file2.file_remote_path,
                                               source_uri=associated_file2.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
@@ -73,11 +69,11 @@ def test_start_upload_happy_path(mock_get_s3_client, get_submission, get_associa
     associated_file_uploader = AssociatedFileUploader(mock_submission_api, 1, False, False, 1)
     associated_file_uploader.start_upload(get_submission, search_folders, resuming_upload)
 
-    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context,
+    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context, submission_id=get_submission.submission_id,
                           resuming_upload=resuming_upload,
                           upload_progress=upload_progress, search_folders=search_folders, num_of_files_not_found=0)
 
-    verify_submission_api(mock_submission_api=mock_submission_api, get_files_by_page_call_ct=2,
+    verify_submission_api(mock_submission_api=mock_submission_api, submission_id=get_submission.submission_id, get_files_by_page_call_ct=2,
                           get_upload_credentials_call_ct=2, batch_update_associated_file_status_call_ct=2)
 
     assert mock_s3_client.upload_file.call_count == 2
@@ -98,12 +94,12 @@ def test_start_upload_files_not_found_reenter(mock_get_s3_client, mock_input, ge
 
     mock_submission_api.get_files_by_page.side_effect = [[associated_file1], [associated_file2], [associated_file2]]
 
-    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file_id1,
+    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file1.id,
                                               destination_uri=associated_file1.file_remote_path,
                                               source_uri=associated_file1.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
                                               session_token='DFDFKSADL3452340980')
-    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file_id2,
+    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file2.id,
                                               destination_uri=associated_file2.file_remote_path,
                                               source_uri=associated_file2.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
@@ -121,11 +117,11 @@ def test_start_upload_files_not_found_reenter(mock_get_s3_client, mock_input, ge
     associated_file_uploader = AssociatedFileUploader(mock_submission_api, 1, False, False, 1)
     associated_file_uploader.start_upload(get_submission, search_folders, resuming_upload)
 
-    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context,
+    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context, submission_id=get_submission.submission_id,
                           resuming_upload=resuming_upload,
                           upload_progress=upload_progress, search_folders=search_folders, num_of_files_not_found=0)
 
-    verify_submission_api(mock_submission_api=mock_submission_api, get_files_by_page_call_ct=3,
+    verify_submission_api(mock_submission_api=mock_submission_api, submission_id=get_submission.submission_id, get_files_by_page_call_ct=3,
                           get_upload_credentials_call_ct=3, batch_update_associated_file_status_call_ct=2)
 
     assert mock_s3_client.upload_file.call_count == 2
@@ -149,12 +145,12 @@ def test_start_upload_files_not_found_exit(mock_get_s3_client, mock_input, mock_
 
     mock_submission_api.get_files_by_page.side_effect = [[associated_file1], [associated_file2]]
 
-    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file_id1,
+    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file1.id,
                                               destination_uri=associated_file1.file_remote_path,
                                               source_uri=associated_file1.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
                                               session_token='DFDFKSADL3452340980')
-    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file_id2,
+    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file2.id,
                                               destination_uri=associated_file2.file_remote_path,
                                               source_uri=associated_file2.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
@@ -171,11 +167,11 @@ def test_start_upload_files_not_found_exit(mock_get_s3_client, mock_input, mock_
     with pytest.raises(SystemExit):
         associated_file_uploader.start_upload(get_submission, search_folders, resuming_upload)
 
-    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context,
+    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context, submission_id=get_submission.submission_id,
                           resuming_upload=resuming_upload,
                           upload_progress=upload_progress, search_folders=search_folders, num_of_files_not_found=1)
 
-    verify_submission_api(mock_submission_api=mock_submission_api, get_files_by_page_call_ct=2,
+    verify_submission_api(mock_submission_api=mock_submission_api, submission_id=get_submission.submission_id, get_files_by_page_call_ct=2,
                           get_upload_credentials_call_ct=2, batch_update_associated_file_status_call_ct=1)
 
     assert mock_s3_client.upload_file.call_count == 1
@@ -196,7 +192,7 @@ def test_start_upload_s3_upload_error(mock_get_s3_client, mock_exit, get_submiss
 
     mock_submission_api.get_files_by_page.side_effect = [[associated_file1]]
 
-    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file_id1,
+    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file1.id,
                                               destination_uri=associated_file1.file_remote_path,
                                               source_uri=associated_file1.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
@@ -213,11 +209,11 @@ def test_start_upload_s3_upload_error(mock_get_s3_client, mock_exit, get_submiss
     with pytest.raises(SystemExit):
         associated_file_uploader.start_upload(get_submission, search_folders, resuming_upload)
 
-    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context,
+    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context, submission_id=get_submission.submission_id,
                           resuming_upload=resuming_upload,
                           upload_progress=upload_progress, search_folders=search_folders, num_of_files_not_found=0)
 
-    verify_submission_api(mock_submission_api=mock_submission_api, get_files_by_page_call_ct=1,
+    verify_submission_api(mock_submission_api=mock_submission_api, submission_id=get_submission.submission_id, get_files_by_page_call_ct=1,
                           get_upload_credentials_call_ct=1, batch_update_associated_file_status_call_ct=0)
 
     assert mock_s3_client.upload_file.call_count == 1
@@ -237,12 +233,12 @@ def test_start_upload_resume_upload(mock_get_s3_client, get_submission, get_asso
 
     mock_submission_api.get_files_by_page.side_effect = [[associated_file1], [associated_file2]]
 
-    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file_id1,
+    upload_creds1 = AssociatedFileUploadCreds(submissionFileId=associated_file1.id,
                                               destination_uri=associated_file1.file_remote_path,
                                               source_uri=associated_file1.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
                                               session_token='DFDFKSADL3452340980')
-    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file_id2,
+    upload_creds2 = AssociatedFileUploadCreds(submissionFileId=associated_file2.id,
                                               destination_uri=associated_file2.file_remote_path,
                                               source_uri=associated_file2.file_user_path, access_key='135DFVDFBNDL',
                                               secret_key='RHGADKVNASDLG4534543534F',
@@ -261,18 +257,18 @@ def test_start_upload_resume_upload(mock_get_s3_client, get_submission, get_asso
     associated_file_uploader = AssociatedFileUploader(mock_submission_api, 1, False, False, 1)
     associated_file_uploader.start_upload(get_submission, search_folders, resuming_upload)
 
-    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context,
+    verify_upload_context(upload_context=associated_file_uploader.uploader.upload_context, submission_id=get_submission.submission_id,
                           resuming_upload=resuming_upload,
                           upload_progress=upload_progress, search_folders=search_folders, num_of_files_not_found=0)
 
-    verify_submission_api(mock_submission_api=mock_submission_api, get_files_by_page_call_ct=2,
+    verify_submission_api(mock_submission_api=mock_submission_api, submission_id=get_submission.submission_id, get_files_by_page_call_ct=2,
                           get_upload_credentials_call_ct=2, batch_update_associated_file_status_call_ct=2)
 
     assert mock_s3_client.upload_file.call_count == 1
     assert mock_s3_client.head_object.call_count == 2
 
 
-def verify_upload_context(upload_context, resuming_upload, upload_progress, search_folders, num_of_files_not_found):
+def verify_upload_context(upload_context, submission_id, resuming_upload, upload_progress, search_folders, num_of_files_not_found):
     assert upload_context.submission.submission_id == submission_id
     assert upload_context.resuming_upload == resuming_upload
     assert upload_context.upload_progress == upload_progress
@@ -282,7 +278,7 @@ def verify_upload_context(upload_context, resuming_upload, upload_progress, sear
     assert len(upload_context.files_not_found) == num_of_files_not_found
 
 
-def verify_submission_api(mock_submission_api, get_files_by_page_call_ct, get_upload_credentials_call_ct,
+def verify_submission_api(mock_submission_api, submission_id, get_files_by_page_call_ct, get_upload_credentials_call_ct,
                           batch_update_associated_file_status_call_ct):
     mock_submission_api.get_upload_progress.assert_called_with(submission_id)
     assert mock_submission_api.get_files_by_page.call_count == get_files_by_page_call_ct
